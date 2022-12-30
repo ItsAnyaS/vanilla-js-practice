f1ab1399a25858821bf56b90e0e7d25ee97514fd
anastasia <anyasirman@outlook.com>
2022-12-30 15:57:33 -0500

    added increase and decrese btn for counter

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const button = document.querySelector('#button')
+const increase = document.querySelector('#increase')
+const decrease = document.querySelector('#decrease')
 const countDisplay = document.querySelector('#counter')
 let buttonCount = 0
 
 
-button.addEventListener('click', e => {
+increase.addEventListener('click', e => {
     buttonCount++
     countDisplay.innerHTML = buttonCount
+})
+
+decrease.addEventListener('click', e => {
+    if (buttonCount >  0){
+        buttonCount--
+        countDisplay.innerHTML = buttonCount
+    }
 })
